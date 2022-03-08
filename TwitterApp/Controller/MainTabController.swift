2b8ef84674a3337a08377b03cf54f33cfa026eea
Giovanna <giovannagmoeller@gmail.com>
2022-03-08 09:05:29 -0300

    creating inicial app structure

--- TwitterApp/Controller/MainTabController.swift
+++ TwitterApp/Controller/MainTabController.swift
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainTabController: UITabBarController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
-    view.backgroundColor = .red
-    title = "Hello!"
+    view.backgroundColor = .systemBlue
   }
 
 
